fe8264fd52b4dd2f1e646124d07d891507fb6ab1
shang8024 <jiale.shang@mail.utoronto.ca>
2022-10-23 16:36:24 -0400

    spending card layout update

--- Frontend/fund-management/src/components/Card/index.js
+++ Frontend/fund-management/src/components/Card/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Card = (props) => {
+    const {spending,fmdMinted,index} = props;
     const [status,setStatus] = React.useState(null);
     const [loading,setLoading] = React.useState(false);
 
@@ -18,19 +19,23 @@ const Card = (props) => {
         <div className="card flex-none">
             <div className="card-content">
                 <div className='title'>
-                    <h2 className="name">Proposal {props.index}</h2>
-                    <h2 className='number'>{props.data.amt}</h2>
+                    <h2 className="name">Proposal {index}</h2>
+                    <div><a>goerli Ξ</a></div>
+                    <h2 className='number'>{spending.amt}</h2>
                 </div>
-                <a>Receiver: {props.data.receiver}</a>
-                <p className="description">{props.data.purpose}</p>
+                <a>Receiver: {spending.receiver}</a>
+                <p className="description">{spending.purpose}</p>
+            </div>
+            <div className='progress'>
+                <h3>{spending.approvalETHCount}/{fmdMinted}</h3>
             </div>
             <div className="card-button-group">
-                {loading ? <div className="loader"/>: ((!status) && (!props.data.executed) ?
+                {loading ? <div className="loader"/>: ((!status) && (!spending.executed) ?
                     <>
                         <button className="button" id='reject' onClick={()=>respondProposal('rejected')}>Reject</button>
                         <button className="button" id='approve' onClick={()=>respondProposal('approved')}>Approve</button>
                     </>
-                : <div className='button-info'><h3>{ (!props.data.executed) ? "You have "+ (props.data.status ? props.data.status : status) + "this proposal." : "Executed"}</h3></div>)
+                : <div className='button-info'><h3>{ (!spending.executed) ? "You have "+ {status} + " this proposal." : "Executed"}</h3></div>)
             }
             </div>
         </div>
